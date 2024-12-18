d4f4059196ec4cde5f2561bfc9add7d8381ea84f
Payam Kamarzarin <payamkamarzarin@yahoo.com>
2024-12-18 16:42:06 +0330

    add template admin

---
+++ template/admin/layouts/footer.php
@@ -0,0 +1,13 @@
+
+</main>
+</div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="../../js/bootstrap.min.js"></script>
+<script src="../../js/mdb.min.js"></script>
+
+</body>
+
+</html>
